201a1fe22f24da6c7a3b0cbeba87461147817c18
Ken de Mello <k6demello28@fastmail.fm>
2023-07-21 19:15:48 -0400

    starting over with MainManager

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,85 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.Events;
-using System.IO;
 
 public class MainManager : MonoBehaviour
 {
-    public static bool GameOver;
-    public static bool GameStarted;
-    public static int CurrentScore;
-    public static int HighScore;
-    public static string CurrentScorePlayer;
-    public static string HighScorePlayer;
+    public Brick BrickPrefab;
+    public int LineCount = 6;
+    public Rigidbody Ball;
 
-    // Make MainManager a singleton
-    public static MainManager Instance;
-    void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-        LoadSession();
-    }
-
-    // Add points to the score. If the resulting point total is greater than
-    // the high score, replace the high score data with this user (will update)
-    public static void AddPoints(int points)
-    {
-        CurrentScore += points;
-        if (CurrentScore > HighScore)
-        {
-            HighScore = CurrentScore;
-            HighScorePlayer = CurrentScorePlayer;
-        }
-    }
-
-    // SaveData class
-    [System.Serializable]
-    class SaveData
-    {
-        public int CurrentScore;
-        public int HighScore;
-        public string CurrentScorePlayer;
-        public string HighScorePlayer;
-    }
+    public Text ScoreText;
+    public GameObject GameOverText;
     
-    public static void SaveSession()
+    private bool m_Started = false;
+    private int m_Points;
+    
+    private bool m_GameOver = false;
+
+    
+    // Start is called before the first frame update
+    void Start()
     {
-        SaveData data = new SaveData();
-
-        data.CurrentScore = CurrentScore;
-        data.CurrentScorePlayer = CurrentScorePlayer;
-        data.HighScore = HighScore;
-        data.HighScorePlayer = HighScorePlayer;
-
-        string json = JsonUtility.ToJson(data);
-
-
-        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+        
+        int[] pointCountArray = new [] {1,1,2,2,5,5};
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+            }
+        }
     }
 
-    // Load the data from the file to persist in between sessions.
-    public static void LoadSession()
-    {    
-        string path = Application.persistentDataPath + "/savefile.json";
-        if (File.Exists(path))
+    private void Update()
+    {
+        if (!m_Started)
         {
-            string json = File.ReadAllText(path);
-            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_Started = true;
+                float randomDirection = Random.Range(-1.0f, 1.0f);
+                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+                forceDir.Normalize();
 
-            CurrentScore = data.CurrentScore;
-            CurrentScorePlayer = data.CurrentScorePlayer;
-            HighScore = data.HighScore;
-            HighScorePlayer = data.HighScorePlayer;
+                Ball.transform.SetParent(null);
+                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+            }
         }
-        GameOver = false;
+        else if (m_GameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    void AddPoint(int point)
+    {
+        m_Points += point;
+        ScoreText.text = $"Score : {m_Points}";
+    }
+
+    public void GameOver()
+    {
+        m_GameOver = true;
+        GameOverText.SetActive(true);
     }
 }
